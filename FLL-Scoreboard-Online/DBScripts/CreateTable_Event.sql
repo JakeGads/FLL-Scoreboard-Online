fd89cfaaa74410feab857a32bcffc6f98dadc44c
Jake Gadaleta <jgadaleta@ceiamerica.com>
2021-10-13 09:35:19 -0400

    added uml diagram

--- FLL-Scoreboard-Online/DBScripts/CreateTable_Event.sql
+++ FLL-Scoreboard-Online/DBScripts/CreateTable_Event.sql
@@ -11,6 +11,8 @@ GO
 CREATE TABLE [dbo].[Event](
     [ID] [int] IDENTITY(1,1) NOT NULL,
     [Text] [varchar(50)] NOT NULL,
+    [User] [int] NOT NULL,
+    CONSTRAINT [FK_User] FOREIGN KEY ([User]) REFERENCES [dbo].[User] ([UserID]),
  CONSTRAINT [PK_Event] PRIMARY KEY CLUSTERED,
 )
 GO
