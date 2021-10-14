db631b630ff29da3cb65ceec215720074635ea5a
Jake Gadaleta <jgadaleta@ceiamerica.com>
2021-10-14 08:23:45 -0400

    added the dacpac

--- FLL-Scoreboard-Online/DBScripts/CreateTable_User.sql
+++ FLL-Scoreboard-Online/DBScripts/CreateTable_User.sql
@@ -7,15 +7,15 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[TblUser](
-    [UserID] [int] IDENTITY(1,1) NOT NULL,
+CREATE TABLE [dbo].[User](
+    [ID] [int] IDENTITY(1,1) NOT NULL,
     [FullName] [varchar](50) NULL,
     [Email] [varchar](50) NULL,
     [Password] [varbinary](128) NULL,
     [Salt] [varbinary](128) NULL,
- CONSTRAINT [PK_TblUser] PRIMARY KEY CLUSTERED
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED
 (
-    [UserID] ASC
+    [ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
