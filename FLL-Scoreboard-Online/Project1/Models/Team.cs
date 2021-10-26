d0984bf814d3bf5df36ef4c8c53722b0bd667baf
Jake Gadaleta <jgadaleta@ceiamerica.com>
2021-10-26 11:24:20 -0400

    added teams jawn

---
+++ FLL-Scoreboard-Online/Project1/Models/Team.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Scoreboard.Models
+{
+    public class Team
+    {
+        List<int> Scores { get; set; }
+        private List<int> orderedScore;
+        public double Average;
+        public string Name { get; set; }
+        public int Number { get; set; }
+
+        public Team(string name, int number, List<int>Scores = null)
+        {
+            this.Name = name;
+            this.Number = number;
+            if (Scores != null)
+            {
+                this.Scores = Scores;
+                this.genAverage();
+            }
+        }
+
+        private void genAverage(int AverageOf = 0)
+        {
+            this.orderedScore = new List<int>(this.Scores);
+            this.orderedScore.Sort();
+            this.orderedScore.Reverse();
+
+            switch (AverageOf)
+            {
+                case 0:
+                    this.Average = this.orderedScore.Sum() / this.orderedScore.Count;
+                    break;
+                case 1:
+                    this.Average = this.orderedScore[0];
+                    break;
+                default:
+                    this.Average = this.orderedScore.Take(AverageOf).Sum() / AverageOf;
+                    break;
+            }
+            
+
+        }
+
+        public void AddScore(int Score, int AverageOf = 0)
+        {
+            this.Scores.Add(Score);
+            this.genAverage(AverageOf)
+        }
+    }
+}
